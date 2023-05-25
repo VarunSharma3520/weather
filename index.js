9e41814e52518151c7c3bb5cb9a08249dd071af6
VarunSharma3520 <110775856+VarunSharma3520@users.noreply.github.com>
2023-05-25 06:10:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-
 const weather_api = async() => {
-    const location = document.getElementById("search").value
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=fe0dba6abc0c4755b9f185936231405&q=`+location+`&aqi=yes`);
-    const jsonData = await response.json();
-    let add = await jsonData.location.name +", " +  await jsonData.location.region + ", " + await jsonData.location.country
-    let temp = await jsonData.current.temp_c
-    let cloud = await jsonData.current.condition.text
-    let icon =await jsonData.current.condition.icon
-    let co = await jsonData.current.air_quality.co.toString()
-    let no2 = await jsonData.current.air_quality.no2.toString()
-    let o = await jsonData.current.air_quality.o3.toString()
-    let so2 = await jsonData.current.air_quality.so2.toString()
-    let pm25 = await jsonData.current.air_quality.pm2_5.toString()
-    let pm10 = await jsonData.current.air_quality.pm10.toString()
-    document.getElementById("temperature").innerHTML=temp+" °C"
-    document.getElementById("cloud").innerHTML=cloud
-    document.getElementById("weathericon").setAttribute("src",icon)
-    document.getElementById("weathericon").setAttribute("class","card-img-top container")
-    document.getElementById("address").innerHTML = add
-    document.getElementById("co").innerHTML = co.slice(0,6)
-    document.getElementById("no").innerHTML = no2.slice(0,6)
-    document.getElementById("o").innerHTML = o.slice(0,6)
-    document.getElementById("so").innerHTML = so2.slice(0,6)
-    document.getElementById("pm25").innerHTML = pm25.slice(0,6)
-    document.getElementById("pm10").innerHTML = pm10.slice(0,6)
+    const location = document.getElementById("search_bar").value;   // Extracts the value from search bar...
+    try{
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=fe0dba6abc0c4755b9f185936231405&q=${location}&aqi=yes`);   //Data from api in form of promise...
+        const jsonData = await response.json(); // Convert promise to json
+        document.getElementById("alert").innerHTML = `<div class="text-center alert alert-success alert-dismissible fade show" role="alert"><strong>Showing details for ${location}</strong><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;    // Return success alert
+        setTimeout(()=>{
+            document.getElementById("alert").innerHTML =""
+        },3000)
+        let add = jsonData.location.name +", " +  jsonData.location.region + ", " + jsonData.location.country   //Stores Value of specific data to be updated
+        let temp = jsonData.current.temp_c  //Stores Value of specific data to be updated
+        let cloud = jsonData.current.condition.text //Stores Value of specific data to be updated
+        let icon =jsonData.current.condition.icon   //Stores Value of specific data to be updated
+        let co = jsonData.current.air_quality.co.toString() //Stores Value of specific data to be updated
+        let no2 = jsonData.current.air_quality.no2.toString()   //Stores Value of specific data to be updated
+        let o = jsonData.current.air_quality.o3.toString()  //Stores Value of specific data to be updated
+        let so2 = jsonData.current.air_quality.so2.toString()   //Stores Value of specific data to be updated
+        let pm25 = jsonData.current.air_quality.pm2_5.toString()    //Stores Value of specific data to be updated
+        let pm10 = jsonData.current.air_quality.pm10.toString() //Stores Value of specific data to be updated
+        document.getElementById("temperature").innerHTML=temp+" °C" // Performs DOM Manipulation
+        document.getElementById("cloud").innerHTML=cloud    // Performs DOM Manipulation
+        document.getElementById("weathericon").setAttribute("src",icon) // Performs DOM Manipulation
+        document.getElementById("weathericon").setAttribute("class","card-img-top container")   // Performs DOM Manipulation
+        document.getElementById("address").innerHTML = add  // Performs DOM Manipulation
+        document.getElementById("co").innerHTML = co.slice(0,6) // Performs DOM Manipulation
+        document.getElementById("no").innerHTML = no2.slice(0,6)    // Performs DOM Manipulation
+        document.getElementById("o").innerHTML = o.slice(0,6)   // Performs DOM Manipulation
+        document.getElementById("so").innerHTML = so2.slice(0,6)    // Performs DOM Manipulation
+        document.getElementById("pm25").innerHTML = pm25.slice(0,6) // Performs DOM Manipulation
+        document.getElementById("pm10").innerHTML = pm10.slice(0,6) // Performs DOM Manipulation
+    }
+    catch(err){
+        document.getElementById("alert").innerHTML = `<div class="text-center alert alert-danger alert-dismissible fade show" role="alert"><strong>Can't find results for : ${location}</strong><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>`;    // Return failure alert
+        setTimeout(()=>{
+            document.getElementById("alert").innerHTML =""
+        },2000)
+    }
 }
